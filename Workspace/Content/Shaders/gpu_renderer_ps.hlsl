8c50f9f5e049920897a737e0fedc83f4c136f16f
planet620 <me@mpolaczyk.pl>
2024-04-25 21:18:38 +0200

    Simple light shader to check normals.

--- Workspace/Content/Shaders/gpu_renderer_ps.hlsl
+++ Workspace/Content/Shaders/gpu_renderer_ps.hlsl
@@ -6,6 +6,14 @@ SamplerState sampler0 : register(s0);
 
 float4 ps_main(VS_Output input) : SV_Target
 {
-    return float4(input.normal_ws * 0.5 + 0.5, 1);
+    const float4 diffuse = { 0.9, 0.9, 0.7, 1.0};
+    const float4 ambient = {0.2, 0.2, 0.2, 1.0};
+    
+    float4 light_loc = {0,10,0,0};
+    float4 light_dir = light_loc - input.position_ws;
+    
+    return ambient + diffuse * saturate(dot(normalize(light_dir), input.normal_ws));
+    
+    //return float4(input.normal_ws * 0.5 + 0.5, 1);
     //return texture0.Sample(sampler0, input.uv);
 }
