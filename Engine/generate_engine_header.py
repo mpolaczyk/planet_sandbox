348bbbb4103a939c63629d7a32f78e2d885f71bb
mpolaczyk <marcin.polaczyk@gmail.com>
2024-12-21 20:54:57 +0100

    engine.h generation script
    Replacing slash with backslash

--- Engine/generate_engine_header.py
+++ Engine/generate_engine_header.py
@@ -8,7 +8,7 @@ def write_h_files(directory, output_file, exclude_keywords):
         file.write("// Header generated automatically by generate_engine_header.py\n")
         file.write("// It contains only header files that export symbols using ENGINE_API\n")
         file.write("// It should be included only by external projects!\n\n")
-        file.write("#include \"core\entry_point.h\"\n")
+        file.write("#include \"core/entry_point.h\"\n")
         for root, dirs, files in os.walk(directory):
             for file_name in files:
                 if file_name.endswith(".h"):
@@ -25,7 +25,7 @@ def write_h_files(directory, output_file, exclude_keywords):
                     except Exception as e:
                         print(f"Error reading file {full_file_path}: {e}")
                         continue
-                    relative_file_path = os.path.relpath(full_file_path, directory)
+                    relative_file_path = os.path.relpath(full_file_path, directory).replace("\\", "/")
                     print(f'#include "{relative_file_path}"')
                     file.write(f'#include "{relative_file_path}"\n')
 
